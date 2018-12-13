461d4882ef8ce40f6ac83e2bb9a034aa13706e88
Panu Ervamaa <panu.ervamaa@frantic.com>
2018-12-14 00:46:28 +0200

    define event component and use it in app component

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Observable } from 'rxjs';
+import { StorageService, Document } from '../storage.service';
 
 @Component({
   selector: 'app-event',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class EventComponent implements OnInit {
+  @Input() doc: Document;
 
-  constructor() { }
+  event$: Observable<{}>;
+  calls$: Observable<Document[]>;
+
+  constructor(private storage: StorageService) { }
 
   ngOnInit() {
+    this.event$ = this.storage.data$(this.doc);
+    this.calls$ = this.storage.docs$('Call', this.doc);
   }
-
 }
